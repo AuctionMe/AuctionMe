6a0cbad908c6ea88aab5e3540adc94741386430e
Alex Mierzejewski <not set>
2021-04-16 20:18:24 -0400

    Added Like and Dislike feature. (A little buggy still)

--- AuctionMe/FeedViewCell.swift
+++ AuctionMe/FeedViewCell.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
 class FeedViewCell: UITableViewCell {
 
@@ -15,6 +17,10 @@ class FeedViewCell: UITableViewCell {
     @IBOutlet weak var interestsLabel: UILabel!
     @IBOutlet weak var prosConsLabel: UILabel!
     @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    
+    @IBOutlet weak var dislikeButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,10 +32,48 @@ class FeedViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    var disliked:Bool = false
+    
+    func setDisliked(_ isDisliked:Bool){
+        disliked = isDisliked
+        if(disliked){
+            dislikeButton.setImage(UIImage(named:"dislike_red"), for: UIControl.State.normal)
+        }else{
+            dislikeButton.setImage(UIImage(named:"dislike"), for: UIControl.State.normal)
+        }
+    }
+    
     @IBAction func dislike(_ sender: Any) {
+        let toBeDisliked = !disliked
+        if(toBeDisliked){
+            self.setDisliked(true)
+        }else{
+            setDisliked(false)
+        }
+    }
+
+    
+    var liked:Bool = false
+    
+    func setLiked(_ isLiked:Bool){
+        liked = isLiked
+        if(liked){
+            likeButton.setImage(UIImage(named:"like_green"), for: UIControl.State.normal)
+        }else{
+            likeButton.setImage(UIImage(named:"like"), for: UIControl.State.normal)
+        }
     }
     
     @IBAction func like(_ sender: Any) {
+        let toBeLiked = !liked
+        if(toBeLiked){
+            self.setLiked(true)
+        }else{
+            setLiked(false)
+        }
     }
+    
+    
+    
 }
